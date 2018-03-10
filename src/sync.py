1c1b2f82d094d1a3f283a44bce8085572747b3a8
Devon Mortimer <devon.mortimer@gmail.com>
2018-03-10 18:15:56 +1300

    Prevent resending received states

--- src/sync.py
+++ src/sync.py
@@ -38,33 +38,44 @@ class State(NamedTuple):
         return json.dumps(self.json())
 
 
+class LocalState:
+    state: State = None
+
+
 async def sync(token: str, reader: StreamReader, writer: StreamWriter,
                *, server: bool=False) -> None:
     connector = aiohttp.TCPConnector(resolver=aiohttp.AsyncResolver())
     async with aiohttp.ClientSession(connector=connector) as session:
         spot = Spotify(session, token)
+        received_state = LocalState()
         await asyncio.wait([
-            publish(writer, spot, server=server) if server else
-            subscribe(reader, spot)
+            publish(writer, spot, received_state, server=server) if server else
+            subscribe(reader, spot, received_state)
         ], return_when=asyncio.FIRST_COMPLETED)
 
 
-async def next_safe_state(spot: Spotify) -> State:
+async def next_safe_state(spot: Spotify, received_state: LocalState
+                          ) -> Optional[State]:
     global lock
 
     data = await spot.get_playing(block=True)
-    if lock.locked():
-        with await lock:
+    was_locked = lock.locked()
+    with await lock:
+        if was_locked:
             data = await spot.get_playing()
-    return State.from_json(data)
+        state = State.from_json(data)
+        if not is_update(state, received_state.state):
+            state = None
+        received_state.state = None
+    return state
 
 
 def encode(state: State) -> bytes:
     return f'{state.serialize()}\n'.encode()
 
 
-async def publish(writer: StreamWriter, spot: Spotify, *, server: bool=False
-                  ) -> None:
+async def publish(writer: StreamWriter, spot: Spotify,
+                  received_state: LocalState, *, server: bool=False) -> None:
     if server:
         logger.info('Publish to newcomer')
         try:
@@ -75,7 +86,9 @@ async def publish(writer: StreamWriter, spot: Spotify, *, server: bool=False
     while True:
         try:
             logger.info('Publish waiting')
-            writer.write(encode(await next_safe_state(spot)))
+            state = await next_safe_state(spot, received_state)
+            if state is not None:
+                writer.write(encode(state))
         except KeyError:
             logger.warning('KeyError during loop')
             pass
@@ -93,7 +106,8 @@ def is_update(new: State, old: State) -> bool:
     )
 
 
-async def safe_update(message: State, spot: Spotify) -> None:
+async def safe_update(message: State, spot: Spotify,
+                      received_state: LocalState) -> None:
     global lock
 
     with await lock:
@@ -102,9 +116,11 @@ async def safe_update(message: State, spot: Spotify) -> None:
             await spot.play(message.uri, message.seek)
             if message.pause:
                 await spot.pause()
+            received_state.state = message
 
 
-async def subscribe(reader: StreamReader, spot: Spotify) -> None:
+async def subscribe(reader: StreamReader, spot: Spotify,
+                    received_state: LocalState) -> None:
     logger.info('Subscribe ready')
     while True:
         try:
@@ -114,7 +130,7 @@ async def subscribe(reader: StreamReader, spot: Spotify) -> None:
             message = State(**json.loads(raw_message))
             logger.info(f'Get {message}')
 
-            await safe_update(message, spot)
+            await safe_update(message, spot, received_state)
         except ConnectionResetError:
             logger.warning('ConnectionResetError during loop')
             return
